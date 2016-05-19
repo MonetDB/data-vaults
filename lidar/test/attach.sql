5731324a1af033db8da9551756bc70a562e77e56
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2016-05-19 14:13:07 +0200

    sample files for LiDAR data vault

---
+++ lidar/test/attach.sql
@@ -0,0 +1,3 @@
+call lidarattach('/ufs/goncalve/MonetDB/current/sql/backends/monet5/vaults/lidar/Tests/C_14FN1.LAZ');
+--call lidarattach('/ufs/goncalve/MonetDB/current/sql/backends/monet5/vaults/lidar/Tests/C_38DN2.LAZ');
+--call lidarattach('/ufs/goncalve/MonetDB/current/sql/backends/monet5/vaults/lidar/Tests/C_38DN2.LAZ', 'test2');
---
+++ lidar/test/load.sql
@@ -0,0 +1,3 @@
+--call lidarload('C_14FN1');
+call lidarload('c_14fn1');
+--call lidarload('test2');
